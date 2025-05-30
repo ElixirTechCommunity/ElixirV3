9a64ab0197b18c1bc77b9ef3e92eaf6579004fd0
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-05-30 22:56:35 +0530

    GFG | test perfomance events  API

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,21 @@ import ChapterSection from "@/components/ChapterSection";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import LogoHero from "@/public/Elixir-logo.png";
-
+import useEventApi from "@/hooks/useEventApi";
 
 const Home = () => {
+  useEventApi();
 
   return (
-    <div className='max-w-7xl relative z-10 w-full -mt-10'>
-      <motion.div className='hidden  md:block absolute -rotate-12 top-96 -right-16  z-10 '>
-        <Image src={LogoHero} height={300} width={300} alt='tect' className='rounded-md' />
+    <div className="max-w-7xl relative z-10 w-full -mt-10">
+      <motion.div className="hidden  md:block absolute -rotate-12 top-96 -right-16  z-10 ">
+        <Image
+          src={LogoHero}
+          height={300}
+          width={300}
+          alt="tect"
+          className="rounded-md"
+        />
       </motion.div>
       <Hero />
       <ChapterSection />
